de46bc20ddb2d71030042ec42cb71cc186cf1132
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2019-02-23 16:59:46 +0100

    Initial Spring Boot server

---
+++ server/src/main/java/pl/ogochi/rate_classes_server/RateClassesServerApplication.java
@@ -0,0 +1,13 @@
+package pl.ogochi.rate_classes_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateClassesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateClassesServerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/pl/ogochi/rate_classes_server/RateClassesServerApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.ogochi.rate_classes_server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RateClassesServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
