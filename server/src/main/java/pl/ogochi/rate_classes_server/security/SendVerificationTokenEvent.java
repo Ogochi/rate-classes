ac5d77f47625ae9535e9a0da1a200c73fcde94e4
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2019-02-25 17:27:33 +0100

    Application event and listener for sending verification email

--- server/src/main/java/pl/ogochi/rate_classes_server/security/SendVerificationTokenEvent.java
+++ server/src/main/java/pl/ogochi/rate_classes_server/security/SendVerificationTokenEvent.java
@@ -5,19 +5,15 @@ import lombok.EqualsAndHashCode;
 import org.springframework.context.ApplicationEvent;
 import pl.ogochi.rate_classes_server.model.User;
 
-import java.util.Locale;
-
 @Data
 @EqualsAndHashCode(callSuper=false)
 public class SendVerificationTokenEvent extends ApplicationEvent {
-    private String appUrl;
-    private Locale locale;
     private User user;
+    private String token;
 
-    public SendVerificationTokenEvent(String appUrl, Locale locale, User user) {
-        super(user);
-        this.appUrl = appUrl;
-        this.locale = locale;
+    public SendVerificationTokenEvent(User user, String token) {
+        super(token);
         this.user = user;
+        this.token = token;
     }
 }
