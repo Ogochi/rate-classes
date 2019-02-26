ec9f42d558130840c6573d2200c0e8df076f8b11
Michał Ołtarzewski <mi.oltarzewski@gmail.com>
2019-02-26 23:28:23 +0100

    Smoke test

--- server/src/test/java/pl/ogochi/rate_classes_server/RateClassesServerApplicationTests.java
+++ server/src/test/java/pl/ogochi/rate_classes_server/RateClassesServerApplicationTests.java
@@ -2,6 +2,10 @@ package pl.ogochi.rate_classes_server;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
